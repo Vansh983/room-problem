597a3ec4962fce0c05149595521a2de6cf8f9121
Vansh Sood <emailofvansh@gmail.com>
2022-09-13 20:09:24 -0300

    setup

---
+++ Machine/LearnProblem.cs
@@ -0,0 +1,11 @@
+﻿namespace RoomProblem.Machine
+{
+    public interface LearnProblem
+    {
+        int NumberOfStates { get; }
+        int NumberOfActions { get; }
+        int[] GetValidActions(int currentState);
+        double GetReward(int currentState, int action);
+        bool GoalStateIsReached(int currentState);
+    }
+}
