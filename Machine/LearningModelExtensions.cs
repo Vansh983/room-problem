dadeedad7b6e9561adc395187a5021a8a71ca48e
Vansh Sood <emailofvansh@gmail.com>
2022-09-15 15:46:51 -0300

    added extensions and printing stats

---
+++ Machine/LearningModelExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace RoomProblem.Machine
+{
+    public static class LearningExtensions
+    {
+        public static double[][] NormalizeMatrix(this double[][] matrix)
+        {
+            var maxElement = matrix.GetMaxElement();
+
+            var normalizedMatrix = new double[matrix.Length][];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                normalizedMatrix[i] = new double[matrix[i].Length];
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] != 0)
+                        normalizedMatrix[i][j] = Math.Round((matrix[i][j] * 100) / maxElement, 0);
+                }
+            }
+            return normalizedMatrix;
+        }
+
+        public static double GetMaxElement(this double[][] matrix)
+        {
+            double maxElement = 0;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] > maxElement)
+                        maxElement = matrix[i][j];
+                }
+            }
+            return maxElement;
+        }
+
+        public static string Print(this double[][] matrix)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    sb.Append(string.Format("{0}   ", matrix[i][j]));
+                }
+                sb.AppendLine(Environment.NewLine);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
